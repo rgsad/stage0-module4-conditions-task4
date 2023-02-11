7a3ef5f52cb2fb48aa6293c263e04ffff77b87be
rgsad <rsad3262@gmail.com>
2023-02-11 22:10:39 +0300

    start task

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int x = first > second ? first : second;
+        int result = x > third ? x : third;
+        System.out.println(result);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int x = first > second ? first : second;
+        int max = x > third ? x : third;
+
+        System.out.println(max);
     }
 }
